7cad67552f749bc8ac23b70afda869c68940cea9
Andy McCorkell <andymccorkell@gmail.com>
2015-08-07 16:32:00 +0100

    mcrypt recipe added

--- composer/recipes/mcrypt.rb
+++ composer/recipes/mcrypt.rb
@@ -1,13 +1,16 @@
 #
-# Cookbook Name:: php-mcrypt
-# Recipe:: default
+# Author::  Panagiotis Papadomitsos (<pj@ezgr.net>)
 #
-# Copyright 2014, Michael Beattie
+# Cookbook Name:: php
+# Recipe:: module_mcrypt
 #
-# Licensed under the MIT License.
+# Copyright 2009-2012, Panagiotis Papadomitsos
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     http://opensource.org/licenses/MIT
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,16 +19,17 @@
 # limitations under the License.
 #
 
-case node[:platform]
-when "ubuntu","debian"
-	include_recipe "php"
-	
-	package "php-mcrypt" do
-	  package_name "php5-mcrypt"
-	  action :install
-	end
+pkg = value_for_platform_family(
+    [ 'rhel', 'fedora' ] => 'php-mcrypt',
+    'debian' => 'php5-mcrypt'
+)
+
+package pkg do
+  action :install
+  notifies(:run, "execute[/usr/sbin/php5enmod mcrypt]", :immediately) if platform?('ubuntu') && node['platform_version'].to_f >= 12.04
 end
 
-template "#{node['php']['ext_conf_dir']}/mcrypt.ini" do
-  mode "0644"
+execute '/usr/sbin/php5enmod mcrypt' do
+  action :nothing
+  only_if { platform?('ubuntu') && node['platform_version'].to_f >= 12.04 && ::File.exists?('/usr/sbin/php5enmod') }
 end
